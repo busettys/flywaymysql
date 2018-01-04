b9c64beb5a0cebe622eb741b2411e43478cc16e3
Sharan Busetty <sbusetty@coca-cola.com>
2018-01-04 16:20:59 +0530

    updated repeatable sql migration script

--- src/main/resources/sql/migrations/R__insert_customer_test_data.sql
+++ src/main/resources/sql/migrations/R__insert_customer_test_data.sql
@@ -1,4 +1,4 @@
 insert into people (first_name, last_name, address_line_1, address_line_2, city, state, zip_code)
-select 'John', 'Doe', '123 Main', 'Apt #2', 'Des Moines', 'IA', '50265'
+select 'Johnp', 'Doep', '123 Mainp', 'Apt #23', 'Des Moinesp', 'IP', '50165'
 union all
-select 'Jane', 'Smith', '789 Spruce', null, 'De Soto', 'IA', '50069';
+select 'TJane', 'Smithv', '789 Spruecee', '46', 'De Sotoo', 'IG', '50099';
