1a9001016522af718481e666bc199e2440aa03e8
Sharan Busetty <sbusetty@coca-cola.com>
2018-01-04 18:29:14 +0530

    updated repeatable sql migration script

--- src/main/resources/functions/R__func1.sql
+++ src/main/resources/functions/R__func1.sql
@@ -3,8 +3,7 @@ SELECT `people`.`person_id`,
     `people`.`last_name`,
     `people`.`phone_number`,
     `people`.`address_line_1`,
-    `people`.`address_line_2`,
-    `people`.`city`,
+      `people`.`city`,
     `people`.`state`,
     `people`.`zip_code`
 FROM `info_dev`.`people`;
